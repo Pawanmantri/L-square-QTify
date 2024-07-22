883f916026135d6657e8373bf3db30a503f47127
Pawanmantri <pawankm10@gmail.com>
2024-07-22 16:11:53 +0530

    remove unwanted sections

--- qtify/src/components/Card/Card.jsx
+++ qtify/src/components/Card/Card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Card.module.css";
+import styles from "./card.module.css";
 import { Chip, Tooltip } from "@mui/material";
 
 function Card({ data, type }) {
